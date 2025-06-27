2728e68c9aba8a26c5f7dc0b04fed6b092a8b408
K3NXXX <xxvolodyax7@gmail.com>
2025-06-27 15:15:27 +0300

    feat: added new project

--- src/lists/projectsList.ts
+++ src/lists/projectsList.ts
@@ -2,11 +2,13 @@ import demoProjectImage1 from '../assets/projects/demoProjectImage1.png'
 import demoProjectImage2 from '../assets/projects/demoProjectImage2.png'
 import demoProjectImage3 from '../assets/projects/demoProjectImage3.png'
 import demoProjectImage4 from '../assets/projects/demoProjectImage4.png'
+import demoProjectImage5 from '../assets/projects/demoProjectImage5.png'
 
-import fullrojectImage1 from '../assets/projects/fullProjectImage1.png'
-import fullrojectImage2 from '../assets/projects/fullProjectImage2.png'
-import fullrojectImage3 from '../assets/projects/fullProjectImage3.png'
-import fullrojectImage4 from '../assets/projects/fullProjectImage4.png'
+import fullProjectImage1 from '../assets/projects/fullProjectImage1.png'
+import fullProjectImage2 from '../assets/projects/fullProjectImage2.png'
+import fullProjectImage3 from '../assets/projects/fullProjectImage3.png'
+import fullProjectImage4 from '../assets/projects/fullProjectImage4.png'
+import fullProjectImage5 from '../assets/projects/fullProjectImage5.png'
 
 export interface IProject {
 	id: number
@@ -31,7 +33,7 @@ export const projectsList: IProject[] = [
 			'Website where you can book suites, tables and meeting rooms',
 		fullDescription:
 			'Website includes booking management, email confirmation during registration and user feedbacks',
-		fullImage: fullrojectImage1,
+		fullImage: fullProjectImage1,
 		demoImage: demoProjectImage1,
 		frontendTechnologies: 'React, SCSS, TypeScript, React Query, MUI',
 		backendTechnologies: 'SQL, Express, TypeScript',
@@ -50,7 +52,7 @@ export const projectsList: IProject[] = [
 		frontendTechnologies:
 			'React, SCSS, Redux Toolkit, TypeScript, React Query, MUI, React Hook Form',
 		backendTechnologies: 'MongoDB, Express, TypeScript',
-		fullImage: fullrojectImage2,
+		fullImage: fullProjectImage2,
 		websiteUrl: 'https://react-tattoo-store.vercel.app/react-tattoo-store',
 		gitHubRepoUrl: 'https://github.com/K3NXXX/react-tattoo-store',
 	},
@@ -64,7 +66,7 @@ export const projectsList: IProject[] = [
 		demoImage: demoProjectImage3,
 		frontendTechnologies: 'React, SCSS, TypeScript, React Query, Firebase, MUI',
 		backendTechnologies: 'None',
-		fullImage: fullrojectImage3,
+		fullImage: fullProjectImage3,
 		websiteUrl: 'https://k3nxxx.github.io/techland-shop/',
 		gitHubRepoUrl: 'https://github.com/K3NXXX/techland-shop',
 	},
@@ -77,8 +79,22 @@ export const projectsList: IProject[] = [
 		demoImage: demoProjectImage4,
 		frontendTechnologies: 'React, SCSS',
 		backendTechnologies: 'None',
-		fullImage: fullrojectImage4,
+		fullImage: fullProjectImage4,
 		websiteUrl: 'https://k3nxxx.github.io/GPT-3/',
 		gitHubRepoUrl: 'https://github.com/K3NXXX/GPT-3',
+	},
+	{
+		id: 5,
+		name: 'Vetra',
+		type: 'React',
+		shortDescription: 'Social network app',
+		fullDescription:
+			'The platform enables users to create posts, like and comment on them, chat in real time, receive instant notifications, follow other users, and more',
+		demoImage: demoProjectImage5,
+		frontendTechnologies: 'React, TypeScript, Zustand, MUI, Socket.IO, i18next',
+		backendTechnologies: 'NestJS, Prisma, TypeScript, Socket.IO',
+		fullImage: fullProjectImage5,
+		websiteUrl: 'https://vetra-social-network-frontend.vercel.app',
+		gitHubRepoUrl: 'https://github.com/K3NXXX/social-network-frontend',
 	},
 ]
